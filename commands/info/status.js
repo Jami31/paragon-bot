b6d0f13dfb272802e4dc2f34f5da75857e60f34d
Jami31 <100648398+Jami31@users.noreply.github.com>
2022-03-05 13:51:02 +0800

    update_v6

---
+++ commands/info/status.js
@@ -0,0 +1,24 @@
+const { client } = require("../..");
+
+module.exports = {
+    name: 'status',
+    description: 'changes the bots status!',
+    execute(Client,message, args) {
+        if (message.author.id !== '948456447560257576') return;
+
+        if (!args[0]) return message.channel.send('Please provide an activity!')
+
+        if (!args[1]) return message.channel.send('Please provide a status!');
+
+        const content = args.slice(1).join('');
+        const type = args[0].toUpperCase();
+
+        client.user.setPresence({
+            activity: {
+                name: content,
+                type: type
+            }
+        })
+        message.channel.send('Done!')
+    }
+}
