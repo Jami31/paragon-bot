4bec1a196603abde14b21e64fbe9ff99c20fb77c
Jami31 <100648398+Jami31@users.noreply.github.com>
2022-03-05 19:43:06 +0800

    test_v1

--- slashcommands/timeout.js
+++ slashcommands/timeout.js
@@ -8,29 +8,32 @@ const durations = [
 	{ name: "1 hour", value: 60 * 60 * 1000 },
 	{ name: "1 day", value: 24 * 60 * 60 * 1000 },
 	{ name: "1 week", value: 7 * 24 * 60 * 60 * 1000 },
-    { name: "1 month, for aisle only", value: 30 * 24 * 60 * 60 * 1000 },
-
 ]
 
 const run = async (client, interaction) => {
     let member = interaction.options.getMember("user")
-	let duration = interaction.options.getNumber("duration")
-	let reason = interaction.options.getString("reason") || "No reason given"
+    let duration = interaction.options.getNumber("duration")
+    let reason = interaction.options.getString("reason") || "No reason given"
+    const newEmbed = new Discord.MessageEmbed()
+        .setColor('#304281')
+        .setTitle('test')
+        .setDescription('this is just a test')
+        .setFooter('test');
 
-	if (!member) return interaction.reply("You must provide a user to timeout")
+    if (!member) return interaction.reply("You must provide a user to timeout")
 
-	try {
-		await member.timeout(duration, reason)
-		 return interaction.reply(
+    try {
+        await member.timeout(duration, reason)
+         return interaction.reply(
 
-		 	`${member.user.tag} has been timed out for ${durations.find((d) => duration === d.value)?.name} with a reason of *${reason}*`
-		 )
-	} catch (e) {
-		if (e) {
-			console.error(e)
-			return interaction.reply(`Failed to timeout ${member.tag}`)
-		}
-	}
+             {embeds: [newEmbed]}
+         )
+    } catch (e) {
+        if (e) {
+            console.error(e)
+            return interaction.reply(`Failed to timeout ${member.tag}`)
+        }
+    }
 }
 
 module.exports = {
