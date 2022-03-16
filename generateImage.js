d8c0ba5a2cfe8d120e261827bf5af4a2f8ed6ef5
Jami31 <100648398+Jami31@users.noreply.github.com>
2022-03-17 05:00:25 +0800

    ver

--- generateImage.js
+++ generateImage.js
@@ -2,14 +2,6 @@ const Canvas = require ("canvas")
 const Discord = require("discord.js")
 const background = "https://i.postimg.cc/dVSpq0PH/frfrwelcome.png"
 
-const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
-    ]
-})
-
 
 const dim = {
     height: 559,
@@ -23,7 +15,7 @@ const av = {
     y: 170
 }
 
-client.on("generateImage", async (member) => {
+const generateImage = async (member) => {
     let username = member.user.username
     let discrim = member.user.discriminator
     let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size})
@@ -36,6 +28,10 @@ client.on("generateImage", async (member) => {
 
     ctx.drawImage(backimg, 0, 0)
 
+    // // draw black tinted box
+    // ctx.fillStyle = "rgba(0,0,0,0.7)"
+    // ctx.fillRect(0,0,1200,675)
+
     const avimg = await Canvas.loadImage(avatarURL)
     ctx.save()
 
@@ -47,23 +43,26 @@ client.on("generateImage", async (member) => {
     ctx.drawImage(avimg, av.x, av.y)
     ctx.restore()
 
+    // // write in text
+    // ctx.fillStyle = "white"
+    // ctx.textAlign = "center"
+
+    // // draw in Welcomg
+    // ctx.font = "70px BankGothic Md BT"
+    // ctx.fillText("W E L C O M E", dim.width/2, dim.margin + 70)
+
+    // // draw in the username
+    // ctx.font = "60px BankGothic Md BT"
+    // ctx.fillText(username + "#" + discrim, dim.width/2, dim.height - dim.margin - 125)
+ 
+    // // draw in to the server
+    // ctx.font = "40px BankGothic Md BT"
+    // ctx.fillText("T O  P A R A G O N", dim.width / 2, dim.height - dim.margin - 50)
 
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
-    const welcomeChannelId = "948138787391799306"
-   
-       const welcomeEmbed = new Discord.MessageEmbed()
+    return attachment
 
-           .setAuthor("Paragon", client.user.displayAvatarURL())
-           .setColor('#2f3136')
-           .setTitle('Welcome!')
-           .setDescription(`Heyyy~ <@${member.id}>, On behalf of the whole \ndepartment,welcome onboard! We believe\n you will bea terrific asset to Roleplayer's Paragon!\n`)
-           .setTimestamp()
-           .setFooter({ text: 'Welcome'})
-           .setThumbnail(client.user.displayAvatarURL())
-           .setImage(`attachment://welcome.png`)
-           .attachFiles(attachment)
-   
-           member.guild.channels.cache.get(welcomeChannelId).send({
-            embeds: [welcomeEmbed]
-        })
-})
+
+}
+
+module.exports = generateImage
